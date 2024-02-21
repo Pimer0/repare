46f4a38de9222139a7993550f03b72755d923207
MERANDON PAUL <paulmerandon@gmail.com>
2024-02-21 21:15:32 +0100

    suppression du bot et mise en pause du projet

--- repare/src/App.jsx
+++ repare/src/App.jsx
@@ -13,7 +13,7 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from "@chatscope/chat-ui-kit-react";
 
 
-const API_KEY = "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"
+const API_KEY = ""
 
 const App = () => {
   const [typing, setTyping] = useState(false)
@@ -86,7 +86,7 @@ await fetch("https://api.openai.com/v1/chat/completions", {
       <DivCTA />
       <DivQui />
       <DivTextFin />
-      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+      {/* <div style={{ position: "relative", height: "800px", width: "700px" }}>
         <MainContainer>
           <ChatContainer>
             <MessageList
@@ -101,7 +101,7 @@ await fetch("https://api.openai.com/v1/chat/completions", {
             <MessageInput placeholder='Écrivez votre message ici' onSend={handleSend} />
           </ChatContainer>
         </MainContainer>
-      </div>
+      </div> */}
       <Footer />
     </div>
   );
